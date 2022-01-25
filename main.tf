8cff7ba120781cd55f6ce7ff1c15e12c888069ea
Andrew Way <andrew.way@env0.com>
2022-01-25 13:40:13 -0800

    Update main.tf
    update version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     env0 = {
       source  = "env0/env0"
-      version = "0.0.20"
+      version = "0.2.24"
     }
   }
   experiments = [module_variable_optional_attrs]
